865c625f2bd64f5cf90b4b69209fe80696bb70ec
narendrapatidargmailcom <narendrapatidar0603@gmail.com>
2024-05-01 17:47:13 +0530

    giving example to jyoti

--- frontend/src/component/Bill.js
+++ frontend/src/component/Bill.js
@@ -1,6 +1,6 @@
 
 
-// // import React, { useState } from 'react';
+//  import React, { useState } from 'react';
 // // import html2pdf from 'html2pdf.js';
 // // import { Typography } from '@mui/material';
 
