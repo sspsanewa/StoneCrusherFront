1bff272f541ad7e632048430722b0928f54f18d4
narendrapatidargmailcom <narendrapatidar0603@gmail.com>
2024-10-30 00:01:07 +0530

    render url change

--- new/src/Config/Url.js
+++ new/src/Config/Url.js
@@ -1,5 +1,5 @@
 // const Url = 'https://meribhiapp.com/2024/musaapp/server/admin'
 
-const Url = 'http://ec2-3-139-102-87.us-east-2.compute.amazonaws.com:5000';
+const Url = 'https://stonecrusher.onrender.com';
 
 export default Url; 
